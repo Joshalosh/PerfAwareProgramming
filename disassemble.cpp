60f235cd8d5249e64a84401400d1525b866bfdc5
Joshalosh <josh.jessopsmith@gmail.com>
2023-03-18 19:38:55 +1100

    Add bool immediate flag to the info struct

--- disassemble.cpp
+++ disassemble.cpp
@@ -47,16 +47,17 @@ struct Instruction {
     u8 mid_bits;
     u8 rm_mask;
     bool reg_on_first_byte;
+    bool is_immediate;
 };
 
 Instruction instruction_table[InstructionType_Count] = {
-    {0b1111'1100, 0b1000'1000, 2, 1, 0b11'000'000, 0b00'111'000, NULL, NULL, 0b00'000'111, false},
-    {0b1111'1110, 0b1100'0110, NULL, 1, 0b11'000'000, 0, 0b00'000'000, 0, 0b00'000'111, false},
-    {0b1111'0000, 0b1011'0000, NULL, 0b0000'1000, NULL, 0b0000'0111, NULL, NULL, NULL, true},
-    {0b1111'1110, 0b1010'0000, NULL, 1, NULL, NULL, NULL, NULL, NULL, false},
-    {0b1111'1110, 0b1010'0010, NULL, 1, NULL, NULL, NULL, NULL, NULL, false},
-    {0b1111'1111, 0b1000'1110, NULL, NULL, 0b11'000'000, NULL, NULL, NULL, 0b00'000'111, false},
-    {0b1111'1111, 0b1000'1100, NULL, NULL, 0b11'000'000, NULL, NULL, NULL, 0b00'000'111, false}};
+    {0b1111'1100, 0b1000'1000, 2, 1, 0b11'000'000, 0b00'111'000, NULL, NULL, 0b00'000'111, false, false},
+    {0b1111'1110, 0b1100'0110, NULL, 1, 0b11'000'000, 0, 0b00'000'000, 0, 0b00'000'111, false, true},
+    {0b1111'0000, 0b1011'0000, NULL, 0b0000'1000, NULL, 0b0000'0111, NULL, NULL, NULL, true, true},
+    {0b1111'1110, 0b1010'0000, NULL, 1, NULL, NULL, NULL, NULL, NULL, false, false},
+    {0b1111'1110, 0b1010'0010, NULL, 1, NULL, NULL, NULL, NULL, NULL, false, false},
+    {0b1111'1111, 0b1000'1110, NULL, NULL, 0b11'000'000, NULL, NULL, NULL, 0b00'000'111, false, false},
+    {0b1111'1111, 0b1000'1100, NULL, NULL, 0b11'000'000, NULL, NULL, NULL, 0b00'000'111, false, false}};
 
 void InitInstructionInfo(Instruction_Info *info, char *ch, int instruction_index,
                          Instruction *instruction_table, Instruction_Type instruction_type)
